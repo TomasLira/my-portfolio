42820356d82e638bd70386843a43e9bf49776317
TomasLira <tomaspaivadelira1@gmail.com>
2025-03-27 12:15:45 -0300

    add

--- static/global.js
+++ static/global.js
@@ -16,38 +16,38 @@ if (currentLink) { // or if (currentLink !== undefined)
     currentLink.classList.add("current");
 }
 */
-const ARE_WE_HOME = document.documentElement.classList.contains("home");
+// const ARE_WE_HOME = document.documentElement.classList.contains("home");
 
-let pages = [
-    {url: "", title: "Home"},
-    {url: "contact/", title: "Contact"},
-    {url: "projects/", title: "Projects"},
-    {url: "resume/", title: "Resume"},
-    {url: "https://github.com/Mike-Kowalski", title: "Github"}
-    // add the rest of your pages here
-];
+// let pages = [
+//     {url: "", title: "Home"},
+//     {url: "contact/", title: "Contact"},
+//     {url: "projects/", title: "Projects"},
+//     {url: "resume/", title: "Resume"},
+//     {url: "https://github.com/Mike-Kowalski", title: "Github"}
+//     // add the rest of your pages here
+// ];
 
-let nav = document.createElement("nav");
-document.body.prepend(nav);
+// let nav = document.createElement("nav");
+// document.body.prepend(nav);
 
-for (let p of pages) {
-    let url = p.url;
-    let title = p.title;
-    // Create link and add it to nav
-    if (!ARE_WE_HOME && !url.startsWith("http")) {
-        url = "../" + url;
-    }
+// for (let p of pages) {
+//     let url = p.url;
+//     let title = p.title;
+//     // Create link and add it to nav
+//     if (!ARE_WE_HOME && !url.startsWith("http")) {
+//         url = "../" + url;
+//     }
 
-    let a = document.createElement("a");
-    a.href = url;
-    a.textContent = title;
+//     let a = document.createElement("a");
+//     a.href = url;
+//     a.textContent = title;
 
-    if (a.host === location.host && a.pathname === location.pathname) {
-        a.classList.add("current");
-    }
+//     if (a.host === location.host && a.pathname === location.pathname) {
+//         a.classList.add("current");
+//     }
 
-    if (a.host != location.host) {
-        a.setAttribute("target","_blank");
-    }
-    nav.append(a);
-}
+//     if (a.host != location.host) {
+//         a.setAttribute("target","_blank");
+//     }
+//     nav.append(a);
+// }
